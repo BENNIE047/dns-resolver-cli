f4e46315cfbf7760173179311f8d72a3a970dd18
BENN-SON <sturdierbenniey@gmail.com>
2026-01-29 21:32:02 +0300

    Update dns-resolve.py

--- dns-resolve.py
+++ dns-resolve.py
@@ -1,5 +1,27 @@
 import socket
 
-web=input("enter the name of the website..eg google.com: ")
-ip=socket.gethostbyname(web)
-print(f"IP ADDRESS IS : {ip}")
+def get_ip_address(hostname):
+    try:
+        ip_address = socket.gethostbyname(hostname)
+        return ip_address
+    except socket.gaierror:
+        return None
+
+def clean_input(user_input):
+    user_input = user_input.replace("https://", "")
+    user_input = user_input.replace("http://", "")
+    user_input = user_input.replace("www.", "")
+    return user_input.strip()
+
+print(" WEBSITE TO IP ADDRESS RESOLVER")
+print("----------------------------------")
+
+website = input("Enter a website name (e.g., google.com): ")
+hostname = clean_input(website)
+
+ip = get_ip_address(hostname)
+
+if ip:
+    print(f" The IP address of {hostname} is: {ip}")
+else:
+    print("Could not resolve the website. Please check the name and try again.")
